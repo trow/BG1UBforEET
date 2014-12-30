1c3b9840839f1b6d13bdbf5bda4eca949e290280
Bryan Dunne <astrobryguy@gmail.com>
2014-12-30 17:00:40 -0600

    Fix Kagain's Quest journal bug for BGEE
    Kagain’s quest journal entry was not being removed in BGEE.   Copied
    file bgKAGAIJ.d to bgeeKAGAIJ.d and added a block:
    
    ADD_TRANS_ACTION ~%KAGAIN_JOINED%~
    BEGIN 0 END
    BEGIN END
    ~EraseJournalEntry(27194)~
    
    Edited TP2 file to compile bgeeKAGAIJ.d for BGEE.
    
    Thanks to RDarkfire for identifying the issue.

--- bg1ub/coran/cpm/wyvern.d
+++ bg1ub/coran/cpm/wyvern.d
@@ -1,3 +1,4 @@
 //Ascension64: makes Coran's wyverns unique
 REPLACE_TRIGGER_TEXT %CORAN_JOINED% ~!Dead("Wyvern")~ ~!Dead("X#CoranWyvern")~
+REPLACE_TRIGGER_TEXT %CORAN_JOINED% ~Dead("Wyvern")~ ~Dead("X#CoranWyvern")~
 ADD_STATE_TRIGGER %tutu_var%KELDDA 6 ~Dead("X#CoranWyvern") InParty("Coran")~
---
+++ bg1ub/kagain/bgeekagaij.d
@@ -0,0 +1,11 @@
+//for BGEE only
+ADD_TRANS_ACTION ~%KAGAIN_JOINED%~
+BEGIN 0 END
+BEGIN END
+~EraseJournalEntry(27194)~
+
+ADD_TRANS_ACTION ~%KAGAIN_JOINED%~
+BEGIN 1 END
+BEGIN 0 END
+~LeaveParty()
+EscapeArea()~
