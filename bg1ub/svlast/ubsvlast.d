7e25fb67edc1e67a908e438be1a350a712571d66
Bryan Dunne <astrobryguy@gmail.com>
2015-11-25 19:23:43 -0600

    Bug fixes
    Fixes for following bugs from ALIEN:
    
    1. ReallyForceSpellRES undefined for BG1:TotSC (?). Use ForceSpellRES
    instead.
    2. Bad path for ublahl.d
    3. FaceObject() action not supported in BG1:TotSC. Creating workaround
    from suggestion by devSin to use "spell that does nothing" to force
    actor to face object. On BG2-engine platforms, still use FaceObject().

--- bg1ub/svlast/ubsvlast.d
+++ bg1ub/svlast/ubsvlast.d
@@ -27,13 +27,13 @@ END
 IF ~Global("UBSVLAST_ENC","GLOBAL",5)~ THEN BEGIN 3 // If revenant is killed 
   SAY @4223 // ~What have you done? Now I am twice as wretched! Kill me in this tortured place! At least there will be some justice then.~ /* #14893 - MODIFIED */
   IF ~~ THEN REPLY @4246 // ~As you wish. Let justice be done.~ 
-  	DO ~SetGlobal("UBSVLAST_ENC","GLOBAL",10) ActionOverride(LastTalkedToBy(Myself),FaceObject("ubsvlast")) Wait(1) Kill("ubsvlast")~ JOURNAL @4295 EXIT
+  	DO ~SetGlobal("UBSVLAST_ENC","GLOBAL",10) ActionOverride(LastTalkedToBy(Myself),%UBFaceObject%"ubsvlast")) Wait(1) Kill("ubsvlast")~ JOURNAL @4295 EXIT
   IF ~~ THEN REPLY @4247 // ~I refuse. I will not be your executioner. You will have to atone for your crimes. Perhaps then others may learn from your mistakes.~ 
   	DO ~SetGlobal("UBSVLAST_ENC","GLOBAL",8)~ JOURNAL @4296 GOTO 4
   IF ~~ THEN REPLY @4248 // ~That's a fine way to show some gratitude! I just saved your miserable life!~
   	DO ~SetGlobal("UBSVLAST_ENC","GLOBAL",8)~ JOURNAL @4297 GOTO 8
   IF ~~ THEN REPLY @4249 // ~If that's what you desire, it will be my pleasure.~
-  	DO ~SetGlobal("UBSVLAST_ENC","GLOBAL",10) ActionOverride(LastTalkedToBy(Myself),FaceObject("ubsvlast")) Wait(1) Kill("ubsvlast")~ JOURNAL @4298 EXIT
+  	DO ~SetGlobal("UBSVLAST_ENC","GLOBAL",10) ActionOverride(LastTalkedToBy(Myself),%UBFaceObject%"ubsvlast")) Wait(1) Kill("ubsvlast")~ JOURNAL @4298 EXIT
 END
 
 IF ~~ THEN BEGIN 4 // from:
