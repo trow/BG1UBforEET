336cd0f2c6d02d9f244e1dc9accc9aca296136b3
Bryan Dunne <astrobryguy@gmail.com>
2015-11-27 14:11:24 -0600

    Unshey dialog updates
    Unshey was only giving resored Girdle of Opposite Gender dialog if the
    cursed character talked to Unshey. This should allow Unshey's restored
    dialog if any party member is wearing the cursed girdle.

--- bg1ub/dialogues/cpm/ubunshey.d
+++ bg1ub/dialogues/cpm/ubunshey.d
@@ -7,7 +7,11 @@ ADD_STATE_TRIGGER ~%tutu_var%unshey~ 5 ~Global("UBHelpUnshey","GLOBAL",1)~
 REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 0 ~Global("UBHelpUnshey","GLOBAL",0) ReactionLT(LastTalkedToBy,NEUTRAL_LOWER)~
 REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 1 ~Global("UBHelpUnshey","GLOBAL",0) ReactionGT(LastTalkedToBy,HOSTILE_UPPER)~
 REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 2 ~Global("UBHelpUnshey","GLOBAL",1) !PartyHasItem("BELT04") !PartyHasItem("BELT05")~
-REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 3 ~Global("UBHelpUnshey","GLOBAL",1) !PartyHasItem("BELT04") PartyHasItem("BELT05") !HasItemEquiped("%tutu_var%BELT05",[PC])~ 
+REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 3 ~Global("UBHelpUnshey","GLOBAL",1) !PartyHasItem("BELT04") PartyHasItem("BELT05") !HasItemEquiped("%tutu_var%BELT05",Player1) !HasItemEquiped("%tutu_var%BELT05",Player2)
+!HasItemEquiped("%tutu_var%BELT05",Player3)
+!HasItemEquiped("%tutu_var%BELT05",Player4)
+!HasItemEquiped("%tutu_var%BELT05",Player5)
+!HasItemEquiped("%tutu_var%BELT05",Player6)~ 
 REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 7 ~Global("UBHelpUnshey","GLOBAL",3)~
 REPLACE_STATE_TRIGGER ~%tutu_var%unshey~ 8 ~Global("UBHelpUnshey","GLOBAL",2)~
 
@@ -18,7 +22,7 @@ ADD_TRANS_ACTION ~%tutu_var%unshey~ BEGIN 6 END BEGIN 0 END ~SetGlobal("UBHelpUn
 SET_WEIGHT ~%tutu_var%unshey~ 3 #-1
 
 APPEND ~%tutu_var%unshey~
-  IF WEIGHT #-1 ~HasItemEquiped("%tutu_var%BELT05",[PC]) Global("UBHelpUnshey","GLOBAL",2)~ THEN BEGIN UBUnsheyGenderGirdle
+  IF WEIGHT #-1 ~Global("UBHelpUnshey","GLOBAL",2) OR(6) HasItemEquiped("%tutu_var%BELT05",Player1) HasItemEquiped("%tutu_var%BELT05",Player2) HasItemEquiped("%tutu_var%BELT05",Player3) HasItemEquiped("%tutu_var%BELT05",Player4) HasItemEquiped("%tutu_var%BELT05",Player5) HasItemEquiped("%tutu_var%BELT05",Player6)~ THEN BEGIN UBUnsheyGenderGirdle
   SAY @0
     IF ~~ THEN  REPLY @1 GOTO 6
     IF ~~ THEN  REPLY @2 EXIT
