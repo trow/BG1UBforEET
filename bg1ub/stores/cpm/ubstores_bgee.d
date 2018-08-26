0f1b46d494a5fda38c72bf2b1947a7c4f50e04a3
AstroBryGuy <AstroBryGuy@users.noreply.github.com>
2018-08-25 23:53:35 -0500

    Use TAV0705.STO for Elfsong
    Updating ALYTH and BART8 to use TAV0705 for the Elfsong Tavern, since it does not have guest rooms.

--- bg1ub/stores/cpm/ubstores_bgee.d
+++ bg1ub/stores/cpm/ubstores_bgee.d
@@ -1,3 +1,11 @@
 REPLACE_ACTION_TEXT ubmerch
   ~StartStore("\(%tutu_var%[Ss][Tt][Oo]4901\)",LastTalkedToBy(\(Myself\)?))~
   ~StartStore("%tutu_var%sto4907",LastTalkedToBy())~
+
+REPLACE_ACTION_TEXT alyth
+  ~StartStore("\([Ii][Nn][Nn]0705\)",LastTalkedToBy(\(Myself\)?))~
+  ~StartStore("tav0705",LastTalkedToBy())~
+
+REPLACE_ACTION_TEXT bart8
+  ~StartStore("\([Ii][Nn][Nn]0705\)",LastTalkedToBy(\(Myself\)?))~
+  ~StartStore("tav0705",LastTalkedToBy())~
