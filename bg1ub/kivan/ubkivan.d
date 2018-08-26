77d16d225c9345d182a05f6ad2708c39e7ce577f
AstroBryGuy <AstroBryGuy@users.noreply.github.com>
2018-08-25 23:46:20 -0500

    Updates for Kivan-Tazok
    Adds Kivan-Tazok recognition dialogues to more states.

--- bg1ub/kivan/ubkivan.d
+++ bg1ub/kivan/ubkivan.d
@@ -3,7 +3,61 @@ I_C_T2 ~%tutu_scriptbg%TAZOK~ 1 UBKivanAndTazok
 == %tutu_scriptbg%TAZOK @1
 END
 
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 6 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 7 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 10 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 12 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 14 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 15 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 20 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 21 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 24 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 26 UBKivanAndTazok
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @0
+== %tutu_scriptbg%TAZOK @1
+END
+
 I_C_T2 ~%tutu_scriptbg%TAZOK~ 13 UBKivanAndTazokLeave
+== %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @2
+END
+
+I_C_T2 ~%tutu_scriptbg%TAZOK~ 27 UBKivanAndTazokLeave
 == %KIVAN_JOINED% IF ~InParty("kivan") !Dead("kivan") !StateCheck("kivan",STATE_SLEEPING)~ THEN @2
 END
 
